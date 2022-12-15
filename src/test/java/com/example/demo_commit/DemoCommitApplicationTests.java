365df773da86ffc4ca16856e6643231c91441610
PhanPhucHau <94149448+PhanPhucHau@users.noreply.github.com>
2022-12-15 13:11:23 +0700

    demo git

---
+++ src/test/java/com/example/demo_commit/DemoCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_commit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCommitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
